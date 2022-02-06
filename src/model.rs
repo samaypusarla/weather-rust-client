613b4279a306b1ea3dc040258da8fad8e072170a
samaypusarla <samaypusarla1@gmail.com>
2022-02-06 00:08:43 -0800

    Made changes to rust client

--- src/model.rs
+++ src/model.rs
@@ -4,7 +4,7 @@
 //   In action: https://app.quicktype.io
 //
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Weather {
@@ -14,4 +14,21 @@ pub struct Weather {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Main {
     temp: f64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserResponse {
+    pub access_token: String,
+    pub expires: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GreetingResponse {
+    pub greeting: String,
 }

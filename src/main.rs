28bc7633246d5978174f769569bcdd656b3941dd
samaypusarla <samaypusarla1@gmail.com>
2022-02-06 00:08:43 -0800

    Made changes to rust client

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ mod model;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // A hard-coded JSON
-    let json = r#"
+  // A hard-coded JSON
+  let json = r#"
             {
               "main": {
                 "temp": 30.94
@@ -11,28 +11,62 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         "#;
 
-    // Deserialize the hardcoded JSON into a Weather struct
-    let weather1: model::Weather = serde_json::from_str(json).unwrap();
+  // Deserialize the hardcoded JSON into a Weather struct
+  let weather1: model::Weather = serde_json::from_str(json).unwrap();
 
-    println!("\nWeather from a JSON we hard-coded locally:\n{:?}", weather1);
+  println!(
+    "\nWeather from a JSON we hard-coded locally:\n{:?}",
+    weather1
+  );
 
-    //
-    // Now that we know we can deserialize a hard-coded JSON into a struct model,
-    // let's see if we can fetch the weather from the backend.
-    //
+  //
+  // Now that we know we can deserialize a hard-coded JSON into a struct model,
+  // let's see if we can fetch the weather from the backend.
+  //
 
-    let client = reqwest::Client::new();
+  let client = reqwest::Client::new();
+  let p = model::User {
+    username: "samay".into(),
+    password: "password".into(),
+  };
 
-    let response = client
-        .get("https://api.openweathermap.org/data/2.5/weather?q=corvallis&appid=b98e3f089c86867862f28236d174368a&&units=imperial")
-        .send()
-        .await?;
+  let res = reqwest::Client::new()
+    .post("http://3.83.49.190:3000/v1/auth")
+    .json(&p)
+    .send()
+    .await?;
 
-    let weather2 = response
-        .json::<model::Weather>()
-        .await?;
+  let js = res.json::<model::UserResponse>().await?;
 
-    println!("\nWeather from openweathermap.org:\n {:?}", weather2);
+  // println!("{:#?}", js);
 
-    Ok(())
+  // println!("Access token: {:#?}", js.access_token);
+  let response = client
+    .get("http://3.83.49.190:3000/v1/weather")
+    .header("Authorization", &js.access_token)
+    .send()
+    .await?;
+
+  let weather2 = response.json::<model::Weather>().await?;
+
+  println!(
+    "\nWeather from server running on ec2 instance:\n {:?}",
+    weather2
+  );
+
+  // fetch greeting
+  let response = client
+    .get("http://3.83.49.190:3000/v1/hello")
+    .header("Authorization", &js.access_token)
+    .send()
+    .await?;
+
+  let greeting = response.json::<model::GreetingResponse>().await?;
+
+  println!(
+    "\nGreeting from server running on ec2 instance:\n {:?}",
+    greeting.greeting
+  );
+
+  Ok(())
 }
